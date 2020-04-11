6bfa0592de6cbbcbfaa80e154b47982968abe42c
HerbertWenisch <herbert.wenisch@t-online.de>
2020-04-11 09:42:39 +0100

    TestFile: BugFix2

--- WarteschlangenTest.java
+++ WarteschlangenTest.java
@@ -2,11 +2,9 @@
 
 public class WarteschlangenTest {
     final int N = 9;
-    PatientenWarteschlange wartezimmer;
-    
     
     public void testeAnmelden(){
-        wartezimmer = new PatientenWarteschlange(N);
+        PatientenWarteschlange wartezimmer = new PatientenWarteschlange(N);
         
         for(int i = 0; i < N + 3; i++){
             wartezimmer.anmelden(new Patient("Patient" + i));
